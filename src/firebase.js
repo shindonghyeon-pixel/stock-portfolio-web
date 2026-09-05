16351e6354ac4f16504d384cacbbe3db8fd01483
shindonghyeon-pixel <stmsdhc@gmail.com>
2026-09-05 17:04:53 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// 아래 firebaseConfig 부분을 아까 복사한 본인의 코드로 완전히 교체하세요!
 const firebaseConfig = {
   apiKey: "AIzaSyB-xxxxxxxxxxxxxxx",
   authDomain: "stock-portfolio-xxxx.firebaseapp.com",
@@ -11,6 +11,8 @@ const firebaseConfig = {
   appId: "1:1234567890:web:abcdef123456"
 };
 
-// Firebase 초기화 및 데이터베이스 연결
 const app = initializeApp(firebaseConfig);
+
+// 데이터베이스와 인증 기능을 모두 내보내어 빌드 에러를 원천 차단합니다.
 export const db = getFirestore(app);
+export const auth = getAuth(app);
